e40171e9797009ab3fff7bf0f1e4d9519dd32295
ziyan <zjames1@vt.edu>
2020-05-10 16:09:02 -0400

    Added level 4

--- main.js
+++ main.js
@@ -7,12 +7,21 @@ function submitPassword() {
   }
 }
 
+function sqlInjection(){
+  var sql = document.getElementById("passwordInput").value;
+  if (sql.includes("OR 1=1")){
+    alert("You got in");
+  } else {
+    alert("Incorrect log in");
+  }
+}
+
 function showAdminPopUp() {
   var response = window.prompt(
     "Requires Admin Password \n(hint: I love da Vigenere! Password-Security)"
   );
   if (response == "heumnwkb") {
-    alert("Yes");
+    window.location.href = "code.html";
   } else {
     alert("Incorrect admin password. Intruder alert!");
   }
